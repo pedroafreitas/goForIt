61fe569448b33dad0f6c9837af4b1b4fbbdc6ee6
pedroafreitas <pedro.afd@discente.ufma.br>
2021-07-19 10:14:38 -0300

    cheguei em pacotes

---
+++ tipos/tiposInterface/tipoInterface.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type curso struct {
+	nome string
+}
+
+func main() {
+	var coisa interface{}
+	fmt.Println(coisa)
+
+	coisa = 3
+	fmt.Println(coisa)
+	
+	type dinamico interface{}
+
+	var coisa2 dinamico = "Opa"
+	fmt.Println(coisa2)
+
+	coisa2 = true
+	fmt.Println(coisa2)
+	//essa interface vazia pode receber outros tipos por ser genérico
+
+	coisa2 = curso{"Golang: explorando a linguagem do Google"}
+}
